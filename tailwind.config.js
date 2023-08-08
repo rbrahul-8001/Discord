01e989cfb807c69d48da5e7af8592c1c744f6774
rbrahul-8001 <rbrahul8001@gmail.com>
2023-08-08 10:06:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'd-blue': '#404EED',
+      },
+    }
+  },
+  plugins: [],
+}
+
